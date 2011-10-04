b39741f539723036148b8663c4af58dc8b55c606
Fabian Buch <buch@synyx.de>
2011-10-04 15:47:34 +0200

    only use timeline tweets to broadcast to irc, not all userstream events

--- lib/tweet_stream.rb
+++ lib/tweet_stream.rb
@@ -4,11 +4,15 @@ class TweetStreamThread
   end
 
   def start
-    TweetStream::Client.new.userstream do |status, client|
+    client = TweetStream::Client.new
+
+    client.on_timeline_status do |status|
       client.stop if @bot.quitting
       @bot.channel_manager.each do |channel|
         channel.msg "[@\00303#{status.user.screen_name}\003] #{status.text}"
       end
     end
+
+    client.userstream
   end
 end
